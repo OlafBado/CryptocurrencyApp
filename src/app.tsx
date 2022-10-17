af42adaff4b0ee76eaaed6d34a053d3fc88553bc
OlafWindrunner <niebiel1@gmail.com>
2022-10-17 12:43:04 +0200

    ts with react integration

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import './style.css'
+import React from 'react';
+import logo from './assets/webpack.svg'
 
 export const a = (title: string) => {
     console.log(title)
@@ -6,4 +8,17 @@ export const a = (title: string) => {
 
 export const b = (title: string) => {
     console.log(title)
+}
+
+interface AppProps {
+    title: string
+}
+
+export const App: React.FC<AppProps> = ({title}) => {
+    return (
+        <div>
+            {title}
+            <img src={logo} alt="" />
+        </div>
+    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import { a } from "./app";
 import { b } from "./app";
+import { App } from "./app";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 a('hello')
 b('hi')
+
+const root = document.getElementById('root')
+
+if (root) {
+    createRoot(root).render(<App title='React with TS!!!'/>)
+}
